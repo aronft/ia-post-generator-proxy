019bce1b087d2a8163780655b2833a14b7b67748
blazte <blazte.aster@gmail.com>
2023-11-23 17:18:53 -0500

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,53 @@
 import express from 'express'
 require('dotenv').config()
-import { TextServiceClient }  from "@google-ai/generativelanguage"
+import { TextServiceClient } from "@google-ai/generativelanguage"
 const { GoogleAuth } = require("google-auth-library");
+const cors = require('cors');
 
 const MODEL_NAME = "models/text-bison-001";
 const API_KEY = process.env.MODEL_LANGUAGE_API_KEY;
 
 const app = express()
 app.use(express.json())
+app.use(cors());
+
 
 const port = 3000;
 
 const client = new TextServiceClient({
-  authClient: new GoogleAuth().fromAPIKey(API_KEY),
+    authClient: new GoogleAuth().fromAPIKey(API_KEY),
 });
 
-app.post('/generate-post', async(req, res) => {
+app.post('/generate-post', async (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*')
     const data = req.body
-   
-    const prompt = `We are a post generator web platform, create a ${data.stely} post style, with a ${data.toneVoice} voice for ${data.platform} platform about ${data.message}`
 
-    
-    const responseApi  = await  client.generateText({
+    console.log(data)
 
-      // required, which model to use to generate the result
-      model: MODEL_NAME,
-      // optional, 0.0 always uses the highest-probability result
-      temperature: 0.7,
-      // optional, how many candidate results to generate
-      candidateCount: 1,
-      prompt: {
-        text: prompt,
-      },
+    const prompt = `Create a post for the ${data.platform} social media with ${data.stely} style and ${data.toneVoice} voice. input: ${data.message}`
+
+    const responseApi = await client.generateText({
+
+        // required, which model to use to generate the result
+        model: MODEL_NAME,
+        // optional, 0.0 always uses the highest-probability result
+        temperature: 0.7,
+        // optional, how many candidate results to generate
+        candidateCount: 1,
+        //   // optional, number of most probable tokens to consider for generation
+        topK: 40,
+        // optional, for nucleus sampling decoding strategy
+        topP: 0.95,
+        maxOutputTokens: 1024,
+        // optional, safety settings
+        safetySettings: [{ "category": "HARM_CATEGORY_DEROGATORY", "threshold": 1 }, { "category": "HARM_CATEGORY_TOXICITY", "threshold": 1 }, { "category": "HARM_CATEGORY_VIOLENCE", "threshold": 2 }, { "category": "HARM_CATEGORY_SEXUAL", "threshold": 2 }, { "category": "HARM_CATEGORY_MEDICAL", "threshold": 2 }, { "category": "HARM_CATEGORY_DANGEROUS", "threshold": 2 }],
+        prompt: {
+            text: prompt,
+        },
     })
 
     if (responseApi[0].candidates == null || responseApi[0].candidates.length === 0) {
+        res.statusCode = 500
         res.send({
             status: 500,
             message: "No result",
@@ -42,7 +55,7 @@ app.post('/generate-post', async(req, res) => {
         })
         return
     }
-
+    res.statusCode = 200
     res.send({
         status: 200,
         message: "Success",
@@ -52,6 +65,3 @@ app.post('/generate-post', async(req, res) => {
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port} `))
-
-
-
